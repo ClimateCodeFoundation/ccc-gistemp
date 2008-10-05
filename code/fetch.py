7a88fd398bbb6df9acc16f2c0ea521e07dbdf00e
drj@pobox.com <drj@pobox.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2008-10-05 15:21:37 +0000

    Added a more formal main to fetch.py
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@8 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- code/fetch.py
+++ code/fetch.py
@@ -6,31 +6,65 @@
 """Script to fetch the inputs required for the GISTEMP program.  The
 inputs on are documented in the gistemp.txt file:
 http://clearclimatecode.org/master/test/GISTEMP/gistemp.txt
+
+Invoke without arguments.
 """
 
-# http://www.python.org/doc/2.4.4/lib/module-urllib.html
-import urllib
+# http://www.python.org/doc/2.4.4/lib/module-getopt.html
+import getopt
 # http://www.python.org/doc/2.4.4/lib/module-sys.html
 import sys
+# http://www.python.org/doc/2.4.4/lib/module-urllib.html
+import urllib
 
-# See
-# http://groups.google.com/group/ccc-gistemp-discuss/web/compiling-gistemp-source?hl=en
-# (But station_inventory.Z corrected to station.inventory.Z)
+def doit(output=sys.stdout):
+    """Single function to download everything."""
 
-noaa = """
-ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v2/v2.mean.Z
-ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v2/v2.temperature.inv
-ftp://ftp.ncdc.noaa.gov/pub/data/ushcn/hcn_doe_mean_data.Z
-ftp://ftp.ncdc.noaa.gov/pub/data/ushcn/station.inventory.Z
-""".split()
+    # See
+    # http://groups.google.com/group/ccc-gistemp-discuss/web/compiling-gistemp-source?hl=en
+    # (But station_inventory.Z corrected to station.inventory.Z)
 
-all = noaa
+    noaa = """
+    ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v2/v2.mean.Z
+    ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v2/v2.temperature.inv
+    ftp://ftp.ncdc.noaa.gov/pub/data/ushcn/hcn_doe_mean_data.Z
+    ftp://ftp.ncdc.noaa.gov/pub/data/ushcn/station.inventory.Z
+    """.split()
 
-output = sys.stdout
+    all = noaa
 
-for url in all:
-    def hook(n, bs, ts):
-        output.write("\r%s %d" % (url, n*bs))
-        output.flush()
-    urllib.urlretrieve(url, url.split('/')[-1], hook)
-    output.write('\n')
+    output = sys.stdout
+
+    for url in all:
+        def hook(n, bs, ts):
+            output.write("\r%s %d" % (url, n*bs))
+            output.flush()
+        urllib.urlretrieve(url, url.split('/')[-1], hook)
+        output.write('\n')
+
+# Guido's main, http://www.artima.com/weblogs/viewpost.jsp?thread=4829
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "", ["help"])
+            for o,a in opts:
+                if o in ('--help',):
+                    print __doc__
+                    return 0
+        except getopt.error, msg:
+             raise Usage(msg)
+        doit()
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        print >>sys.stderr, "for help use --help"
+        return 2
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())

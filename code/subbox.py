e3ef387c8bb0a928edb71a766b6aa43435cc661f
drj@pobox.com <drj@pobox.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2009-10-27 16:12:05 +0000

    minor edits; moving comments into docstrings.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@69 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- code/subbox.py
+++ code/subbox.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+# $URL$
+# $Rev$
 # 
 # subbox.py
-# $Id: //info.ravenbrook.com/project/ccc/master/code/subbox.py#2 $
-# Copyright (C) 2008 Ravenbrook Limited.  See end of file for license.
+# Copyright (C) 2009 Ravenbrook Limited.  See end of file for license.
 #
 # For reading trimmed big-endian GISTEMP sub-box data files.
 #
@@ -15,38 +16,45 @@ w = len(struct.pack('=I', 0))
 wf = len(struct.pack('f', 0.0))
 header_length = 8*w
 
-# Each line in a trimmed sub-box file is converted into one of these
-# objects.  It's a subclass of tuple; the elements of the tuple are
-# the data items.  The per-line metadata (latitude, longitude, etc)
-# becomes attributes of the object.
-
 class subboxline(tuple):
+    """Each line in a trimmed sub-box file is converted into a subboxline
+    instance.  It's a subclass of tuple; the elements of the tuple are
+    the data items.  The per-line metadata (latitude, longitude, and
+    so on) become attributes of the object.
+    """
+
     def __new__(cls, record, **kwargs):
         items = (len(record)-header_length)//wf
-        return tuple.__new__(cls, struct.unpack('>%df' % items, record[header_length:]))
+        return tuple.__new__(cls,
+            struct.unpack('>%df' % items, record[header_length:]))
         
     def __init__(self, record, bos='>'):
         keys = 'n lat_S lat_N lon_W lon_E stations station_months d'.split()
         self.bos = bos
-        self.__dict__.update(zip(keys, struct.unpack('%s7if' % bos, record[:header_length])))
+        self.__dict__.update(zip(keys,
+                                 struct.unpack('%s7if' % bos,
+                                               record[:header_length])))
         for k in ('lat_S','lat_N','lon_W','lon_E'):
             self.__dict__[k] /= 100.0
 
     def __repr__(self):
         return ('<subboxline (%+06.2f,%+06.2f) (%+07.2f,%+07.2f): %d>' %
                 (self.lat_S, self.lat_N, self.lon_W, self.lon_E, self.n))
-    
-# Given an open file object, returns the header dict and
-# a generator for the line objects.
 
 def subboxfile(file, bos='>'):
+    """Given an open file object, returns the header dict and
+    a generator for the line objects.
+    """
+
     fortfile = fort.File(file, bos=bos)
     headerline = fortfile.readline()
-    keys = 'mo1 kq mavg monm monm4 iyrbeg missing_flag precipitation_flag title'.split()
+    keys = '''mo1 kq mavg monm monm4 iyrbeg
+              missing_flag precipitation_flag title'''.split()
+    # See SBBXotoBX.f line 79, 77, 113
     header = dict(zip(keys, struct.unpack('>8i80s', headerline)))
     return (header, (subboxline(l, bos=bos) for l in fortfile))
 
-# This file is copyright (C) 2008 Ravenbrook Limited.
+# This file is copyright (C) 2009 Ravenbrook Limited.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are

14c9e281e9fd33b2beabf8143edeb309b08f7ab8
drj@pobox.com <drj@pobox.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2009-10-30 13:56:47 +0000

    converted subbox.py to classes and added SBBXtotext that uses it.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@71 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

---
+++ code/SBBXtotext.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# $URL$
+# $Rev$
+# SBBXtotext.py
+#
+# Convert GISTEMP subbox file to text.
+#
+# David Jones, Ravenbrook Limited
+#
+# Similar job to subboxtotext.py, but this file uses the subbox module.
+# subboxtotext.py is more complete (it automagically handles
+# trimmed/untrimmed for example) but does not use the subbox module.
+# The plan is for this tool (SBBXtotext) to evolve into the preferred tool.
+
+# Ravenbrook
+import subbox
+
+def totext(inp, out, metaonly=False, bos='>', trimmed=None):
+    """Display a GISTEMP subbox file in text format. *metaonly*, when
+    True, means that only the metadata for each subbox record (basically
+    its location on Earth and its length) are displayed.  *bos* specifies the
+    Byte Order and Size: '>' for big-endian (GISS), '<' for
+    little-endian. *trimmed* is not used yet.
+    """
+
+    boxes = subbox.File(inp, bos=bos)
+    print >> out, repr(boxes)
+    for cell in boxes:
+        print >> out, cell
+        if metaonly:
+            continue
+        print >> out, cell[:]
+
+def main(argv=None):
+    import getopt
+    import sys
+
+    if argv is None:
+        argv = sys.argv
+
+    metaonly = False
+    bos = '>'
+    trimmed = 'fromfile'
+    opt, arg = getopt.getopt(argv[1:], 'b:mtu')
+    for o,v in opt:
+        if o == '-m':
+            metaonly = True
+        if o == '-b':
+            bos = v
+        if o == '-t':
+            trimmed = True
+        if o == '-u':
+            trimmed = False
+    if len(arg) == 0:
+        totext(sys.stdin, metaonly=metaonly, bos=bos, trimmed=trimmed)
+    else:
+        for n in arg:
+            totext(open(n, 'rb'), sys.stdout, metaonly=metaonly, bos=bos,
+              trimmed=trimmed)
+
+if __name__ == '__main__':
+    main()
--- code/subbox.py
+++ code/subbox.py
@@ -10,14 +10,15 @@
 # Nick Barnes, Ravenbrook Limited
 
 import struct
+# Ravenbrook
 import fort
 
 w = len(struct.pack('=I', 0))
 wf = len(struct.pack('f', 0.0))
 header_length = 8*w
 
-class subboxline(tuple):
-    """Each line in a trimmed sub-box file is converted into a subboxline
+class Line(tuple):
+    """Each line in a trimmed sub-box file is converted into a Line
     instance.  It's a subclass of tuple; the elements of the tuple are
     the data items.  The per-line metadata (latitude, longitude, and
     so on) become attributes of the object.
@@ -26,7 +27,8 @@ class subboxline(tuple):
     def __new__(cls, record, **kwargs):
         items = (len(record)-header_length)//wf
         return tuple.__new__(cls,
-            struct.unpack('>%df' % items, record[header_length:]))
+            struct.unpack('%s%df' % (kwargs['bos'], items),
+                          record[header_length:]))
         
     def __init__(self, record, bos='>'):
         keys = 'n lat_S lat_N lon_W lon_E stations station_months d'.split()
@@ -38,21 +40,35 @@ class subboxline(tuple):
             self.__dict__[k] /= 100.0
 
     def __repr__(self):
-        return ('<subboxline (%+06.2f,%+06.2f) (%+07.2f,%+07.2f): %d>' %
+        return ('<subbox.Line (%+06.2f,%+06.2f) (%+07.2f,%+07.2f): %d>' %
                 (self.lat_S, self.lat_N, self.lon_W, self.lon_E, self.n))
 
-def subboxfile(file, bos='>'):
-    """Given an open file object, returns the header dict and
-    a generator for the line objects.
+class File:
+    """Represents a GISTEMP subbox file. The file's metadata appears as
+    instance properties.  The sequential file data is available by using
+    the instance as an iterator.
     """
 
-    fortfile = fort.File(file, bos=bos)
-    headerline = fortfile.readline()
-    keys = '''mo1 kq mavg monm monm4 iyrbeg
-              missing_flag precipitation_flag title'''.split()
-    # See SBBXotoBX.f line 79, 77, 113
-    header = dict(zip(keys, struct.unpack('>8i80s', headerline)))
-    return (header, (subboxline(l, bos=bos) for l in fortfile))
+    def __init__(self, file, bos='>'):
+        """Takes an open file object."""
+
+        self.fortfile = fort.File(file, bos=bos)
+        self.bos = bos
+        headerline = self.fortfile.readline()
+        keys = '''mo1 kq mavg monm monm4 iyrbeg
+                  missing_flag precipitation_flag title'''.split()
+        # See SBBXotoBX.f line 79, 77, 113
+        self.__dict__.update(zip(keys, struct.unpack('>8i80s', headerline)))
+
+    def __repr__(self):
+        return '<subbox.File %r' % self.__dict__
+
+    def __iter__(self):
+        return self.next()
+
+    def next(self):
+        for l in self.fortfile:
+            yield Line(l, bos=self.bos)
 
 # This file is copyright (C) 2009 Ravenbrook Limited.
 #

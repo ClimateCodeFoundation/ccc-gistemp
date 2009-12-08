53b9cb067bdf4cfca130560e66e21aea4c59534d
drj@pobox.com <drj@pobox.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2009-12-08 22:54:09 +0000

    vischeck: multiple arguments, used to compare series.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@88 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- code/vischeck.py
+++ code/vischeck.py
@@ -8,6 +8,9 @@
 #
 # Script to produce a visual check of the results (of STEP 5).
 
+class Error(Exception):
+    """Some sort of problem."""
+
 def asann(f):
     """Convert the text file *f* into a sequence of global anomalies.  An
     input file is expected to be one of the NH.*, SH.*, or GLB.* files
@@ -33,14 +36,47 @@ def asann(f):
             except ValueError:
                 yield((year, None))
 
-def asgooglechartURL(i):
-    """Convert an iterable (assumed to be output from :meth:`asann`) into
-    a URL suitable for passing to the Google Chart API.
+def asgooglechartURL(seq):
+    """*seq* is a sequence of iterables (each one assumed to be the
+    output from :meth:`asann`) into a URL suitable for passing to the
+    Google Chart API.
+
+    Each element of the sequence corresponds to a different data series,
+    all the series are plotted on the same chart.
     """
 
     prefix = 'http://chart.apis.google.com/chart'
 
-    l = list(i)
+    ds = []
+    oy = None
+    for i in seq:
+        l = list(i)
+        d = chartsingle(l)
+        ds.append(d)
+        # Let y be the list of years for the chart legend.  We include
+        # the first year of the series, the last year, and every decade
+        # beginning.
+        y = map(lambda x:x[0], l)
+        for i in range(1,len(y)-1):
+            if y[i]%10:
+                y[i]=''
+        if oy is not None and oy != y:
+            raise Error("Year ranges in data series are different.")
+        oy = y
+
+    xaxis = '|' + '|'.join(map(str, y))
+    vaxis = '||-0.5|+0.0|+0.5|'
+    chxl = 'chxl=0:'+xaxis+'|1:'+vaxis+'|2:'+vaxis
+    chxt = 'chxt=x,y,r'
+    chd='chd=t:' + '|'.join(ds)
+    chs='chs=600x500'
+    colours = ['ff0000','000000']
+    chco = 'chco=' + ','.join(colours)
+    return prefix + '?' + '&'.join(['cht=lc',chd,chxt,chxl,chco,chs])
+
+def chartsingle(l):
+    """Take a list and return a URL fragment for its Google
+    chart."""
 
     d = map(lambda x:x[1], l)
 
@@ -57,32 +93,26 @@ def asgooglechartURL(i):
         return x
 
     d = map(scale, d)
-    y = map(lambda x:x[0], l)
-    for i in range(1,len(y)-1):
-        if y[i]%10:
-            y[i]=''
-    xaxis = '|' + '|'.join(map(str, y))
-    vaxis = '||-0.5|+0.0|+0.5|'
-    chxl = 'chxl=0:'+xaxis+'|1:'+vaxis+'|2:'+vaxis
-    chxt = 'chxt=x,y,r'
-    chd='chd=t:' + ','.join(map(str, d))
-    chs='chs=600x500'
-    chco='chco=ff0000'
-    return prefix + '?' + '&'.join(['cht=lc',chd,chxt,chxl,chco,chs])
+    return ','.join(map(str, d))
 
-def chartit(f):
-    """Convert the file object *f* to a Google Chart API url and print it
-    on standard output, and *ahem* write the PNG received from the same
-    URL to a file 'foo.png' where its name is derived from the input
-    file.
+def chartit(fs):
+    """Convert the list of files *fs* to a Google Chart API url and print it
+    on standard output.
+    
+    If the list is exactly one element long then a PNG file received
+    from the URL is written to a file 'foo.png' where its name is
+    derived from the input file.
     """
 
     import re
     import urllib
 
-    url = asgooglechartURL(asann(f))
+    url = asgooglechartURL(map(asann, fs))
     print url
     uf = urllib.urlopen(url)
+    if len(fs) != 1:
+        return
+    f = fs[0]
     # Open a PNG output file with a name based on the input.
     # 'foo.txt' -> 'foo.png' ; '<stdin>' -> '<stdin>.png'
     o = open(re.sub('(\.[^.]*|)$', '.png', f.name), 'wb')
@@ -102,8 +132,7 @@ def main(argv=None):
         fs = map(open, argv[1:])
     else:
         fs = [sys.stdin]
-    for f in fs:
-        chartit(f)
+    chartit(fs)
 
 if __name__ == '__main__':
     main()

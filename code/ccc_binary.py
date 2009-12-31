294b8b18494f75fa22871c1ffcf018ea6cbd9e33
paul@cleversheep.org <paul@cleversheep.org@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2009-12-31 16:24:43 +0000

    Added python versions of the STEP4 fortran programs.
    These use some of the updates to ccc_binary.
    
    
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@131 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- code/ccc_binary.py
+++ code/ccc_binary.py
@@ -10,8 +10,42 @@ import copy
 
 import fort
 
+def prop(method):
+    """Property decorator that keeps documentation.
+
+    This simply invokes the ``property`` built-in function, but it passes the
+    method's docstring as well; making generated API documentation more useful.
+
+    :Param method:
+        The method to convert to a property.
+
+    """
+    return property(method, doc=method.__doc__)
+
+
+def mprop(attrExpr):
+    """Like `prop`, but supports simple modification.
+
+    Simple modification is supported, i.e. assignment of the underlying
+    attribute.
+
+    :Param attrExpr:
+        An partial expression that can be used to set the property,
+        as in self.<attrExpr> = v.
+
+    """
+    x = 'def doSet(self, value): self.%s = value' % attrExpr
+    d = {}
+    exec x in locals(), d
+    doSet = d["doSet"]
+
+    def makeProp(method):
+        return property(fget=method, fset=doSet, doc=method.__doc__)
+    return makeProp
+
+
 class CCBin(object):
-    """Base class for binary stile file records.
+    """Base class for binary style file records.
     
     This simply abstracts out the common code from `CCHeader` and `CCRecord`.
     All clases derived from this class need to provide a member/property called
@@ -20,38 +54,78 @@ class CCBin(object):
     `setFromElements` and `initEmpty` methods.
 
     """
-    def __init__(self, data=None, f=None):
+    def __init__(self, data=None, f=None, bos="@"):
+        """Constructor:
+
+        :Param data:
+            Binary data used to set the contents. See `setFromBinary` for more
+            details.
+
+        :Param f:
+            Not currently used. It is intended to allow the contents to be set
+            by reading from a `fort.File`.
+
+        """
+        self._bos = bos
         if data is not None:
             self.setFromBinary(data)
-        elif f is not None:
+        elif f is not None: #pragma: unsupported
             # TODO: Should provide proper error set
             raise Exception("Construction from file not (yet) supported")
         else:
             self.initEmpty()
 
     def copy(self):
-        """Return a copy of this instance"""
+        """Copy this instance to make a new record.
+        
+        This performs a deep copy so you can modify the copy without affecting
+        the original.
+
+        :Return:
+            A new copy of the record.
+
+        """
         return copy.deepcopy(self)
 
     def setFromBinary(self, data):
-        elements = struct.unpack(self._format, data[:self.size])
+        """Set the contents from a binary string.
+
+        :Param data:
+            The binary data to use to set the content. This *must* be a string
+            and be at least as long as the string returned by the `binary`
+            property.
+
+        """
+        elements = struct.unpack(self._bos + self._format, data[:self.size])
         self.setFromElements(elements)
 
     @property
     def binary(self):
+        """The record as a binary string.
+
+        This is as the record will be when written to a Fortran binary file, but
+        excludes record header and trailer.
+
+        """
         try:
-            return struct.pack(self._format, *self.getElements())
-        except struct.error, exc:
+            return struct.pack(self._bos + self._format, *self.getElements())
+        except struct.error, exc: #pragma: debug
             raise ValueError("%s\n  Provided %d elements" % (
                 exc, len(self.getElements())))
 
 
     @property
     def size(self):
-        return struct.calcsize(self._format)
+        """The size of the record, when converted to a binary string.
+
+        This is functionally equivalent to len(self.binary), but should be
+        quicker.
+
+        """
+        return struct.calcsize(self._bos + self._format)
 
     #{Interface methods
-    def initEmpty(self):
+    def initEmpty(self): #pragma: unsupported
         """Initialise the record as empty.
 
         This is used when a record is created without any source data.
@@ -61,7 +135,7 @@ class CCBin(object):
         raise NotImplementedError("Needs implementing in %s"
                 % self.__class__.__name__)
 
-    def getElements(self):
+    def getElements(self): #pragma: unsupported
         """Return list of data elements.
 
         The returned list is used to write binary records. This list must be
@@ -71,7 +145,7 @@ class CCBin(object):
         raise NotImplementedError("Needs implementing in %s"
                 % self.__class__.__name__)
 
-    def setFromElements(self, elements):
+    def setFromElements(self, elements): #pragma: unsupported
         """Set the record's members from a list of elements.
   
         This is the inverse of `getElements`. The elements will typically have come
@@ -102,9 +176,43 @@ class CCHeader(CCBin):
         +-----------+
      
     This class make accessing the header in the Python world relatively clean
-    and easy. 
+    and easy.
+
+    The header itself has the following structure::
+
+        +-----------+ - - - - - - - - +-------------------+
+        | info[9]   |              0  |  m1               |
+        +-----------+ -               +-------------------+
+        | title[80] |   \          1  |  kq               |
+        +-----------+    .            +-------------------+
+                         |         2  |  mavg             |
+                         .            +-------------------+
+                         |         3  |  monm             |
+                         .            +-------------------+
+                         |         4  |  recsize          |
+                         .            +-------------------+
+                         |         5  |  yrbeg            |
+                         .            +-------------------+
+                         |         6  |  bad              |
+                         .            +-------------------+
+                         |         7  |  trace            |
+                         .            +-------------------+
+                          \        8  |  m2               |
+                            - - - - - +-------------------+
+
+    The `title` should always be left justified and padded with spaces.
+    The `title` property enforces this.
+
+    In the Fortran, the info structure indices start at 1.
+
+    .. Note::
+        The names of the elements of the ``info`` are based on names used
+        within certain Fortran files. The Fortran code does not have 
+        consitent naming conventions and it is *very* likeky better names will
+        emerge as the CCC code evolves.
 
     """
+    # The header format is 9 integers followed by an 80 character string.
     _format = "9i80s"
     def setFromElements(self, elements):
         self.info, self.title = list(elements[:9]), elements[9]
@@ -116,13 +224,175 @@ class CCHeader(CCBin):
     def getElements(self):
         return self.info + [self.title]
 
+    def _set_title(self, s):
+        self._title = s.ljust(80)
+
+    def _get_title(self):
+        return self._title.rstrip()
+
+    title = property(_get_title, _set_title,
+        doc="""The title of the data file.
+
+        This is a plain string which is left justified and automaticlly padded
+        wih spaces.""")
+
     @property
     def m1(self):
+        """The month (or year) number of the first idata element in the
+        following record."""
         return self.info[0]
 
     @property
     def m2(self):
+        """The month (or year) number of the last idata element in the
+        following record."""
         return self.info[8]
+
+    @property
+    def kq(self):
+        """Don't know yet!"""
+        return self.info[1]
+
+    @property
+    def mavg(self):
+        """Don't know yet!"""
+        return self.info[2]
+
+    @property
+    def monm(self):
+        """Don't know yet!"""
+        return self.info[3]
+
+    @property
+    def recsize(self):
+        """Don't know yet!"""
+        return self.info[4]
+
+    @property
+    def yrbeg(self):
+        """Don't know yet!"""
+        return self.info[5]
+
+    @property
+    def bad(self):
+        """Don't know yet!"""
+        return self.info[6]
+
+    @property
+    def trace(self):
+        """Don't know yet!"""
+        return self.info[7]
+
+
+class SBBX_Header(CCBin):
+    """The header for SBBX files.
+
+    This is very similar to `CCHeader`., but the info structure only
+    has 8 elements rather than 9.
+
+    The info elements are (see code/STEP4_5/trimSBBX.f for original
+    details)::
+
+        +-------------------+
+      0 |  m1               | [Note 1]
+        +-------------------+
+      1 |  kq               | quantity flag [Note 2]
+        +-------------------+
+      2 |  mavg             | time avg flags [Note 3]
+        +-------------------+
+      3 |  nm               | length of each time record
+        +-------------------+
+      4 |  recsize          | size of data record length
+        +-------------------+
+      5 |  yrbeg            | first year of each time record
+        +-------------------+
+      6 |  bad              | value used to indicate missing data
+        +-------------------+
+      7 |  trace            | flag for precipitation trace
+        +-------------------+
+
+    Note 1:
+        This is set to ``1`` to indicate that the file is not
+        trimmed/reorganized.
+
+    Note 2:
+        The quantity flag values are not defined in
+        ``code/STEP4_5/trimSBBX.f``.
+
+    Note 3:
+        The time average flag values are:
+*           1-4  => DJF - SON
+            5    => ANN
+            6    => MONTHLY
+            7    => SEAS
+            8-19 =>JAN-DEC
+
+    """
+    # The header format is 8 integers followed by an 80 character string.
+    _format = "8i80s"
+    def setFromElements(self, elements):
+        self.info, self.title = list(elements[:8]), elements[8]
+
+    def initEmpty(self):
+        self.info = [0] * 8
+        self.title = ""
+        
+    def getElements(self):
+        return self.info + [self.title]
+
+    def _set_title(self, s):
+        self._title = s.ljust(80)
+
+    def _get_title(self):
+        return self._title.rstrip()
+
+    title = property(_get_title, _set_title,
+        doc="""The title of the data file.
+
+        This is a plain string which is left justified and automaticlly padded
+        wih spaces.""")
+
+    @mprop("info[0]")
+    def m1(self):
+        """Provisional: The month (or year) number of the first idata element
+        in the following record."""
+        return self.info[0]
+
+    @mprop("info[7]")
+    def trace(self):
+        """Flag for precipitation trace."""
+        return self.info[7]
+
+    @mprop("info[1]")
+    def kq(self):
+        """Quantity flag."""
+        return self.info[1]
+
+    @mprop("info[2]")
+    def mavg(self):
+        """Time average flags."""
+        return self.info[2]
+
+    @mprop("info[3]")
+    def nm(self):
+        """Length of each time record."""
+        return self.info[3]
+
+    @mprop("info[4]")
+    def recsize(self):
+        """Size of data record length."""
+        return self.info[4]
+
+    @mprop("info[5]")
+    def yrbeg(self):
+        """First year of each time record."""
+        return self.info[5]
+
+    @mprop("info[6]")
+    def bad(self):
+        """Value used to indicate missing data."""
+        return self.info[6]
+
 
 class CCRecord(CCBin):
     """Holds the data for a binary temperature record.
@@ -170,9 +440,9 @@ class CCRecord(CCBin):
 class BufferedOutputRecordFile(object):
     """Simple buffer for output records.
 
-    Some of the CCC code generates file where each record contins information
-    about the following record. This naturally means that, effectively,
-    two records need to be loaded at once:
+    Much of the CCC code generates file where each record contains information
+    about the following record. This naturally means that, effectively, two
+    records need to be loaded in memory at once:
 
     - The records that is almost ready to be written.
     - The next rcord, which is needed to fill in a few details in the other
@@ -188,12 +458,12 @@ class BufferedOutputRecordFile(object):
     to be retrieved and modified using the `lastRecord` method. In practice it
     does this by buffering records and never committing the most recent record
     to the underlying file until either:
-
+ 
+    - The `flush` method is invoked with arguments low=0, high=0 (the defaults)
+    - The `RecordFile` is destroyed.
+ 
     Although this uses the `fort.File` class, it expects to buffer and write
     `CCBin` instances, not strings.
-
-    - The `flush` method is invoked.
-    - The `RecordFile` is destroyed.
 
     """
     def __init__(self, path):
@@ -221,14 +491,14 @@ class BufferedOutputRecordFile(object):
 
         """
         self._buffer.append(record)
-        self._pos += self.w * 2 + record.size
+        self._pos += self.f.w * 2 + record.size
         self.flush(low=1, high=10)
 
     def lastRecord(self):
         """Return the last record written, but not flushed.
 
         :Return:
-            The las record written if any. If no record has been written since
+            The last record written if any. If no record has been written since
             the last time all records were flushed then it returns ``None``.
 
         """
---
+++ code/trimSBBX.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+"""Python replacement for code/STEP4_5/trimSBBX.f
+
+Input files:
+
+    tbd
+
+Output files:
+
+    tbd
+
+"""
+__docformat__ = "restructuredtext"
+
+
+import sys
+import struct
+
+import fort
+import ccc_binary
+import script_support
+
+
+km0 = 12
+monm0 = km0 * (3000 - 1850 + 1)
+nsbbx = 8000
+
+
+class Rec(ccc_binary.CCBin):
+    """Holds input file records.
+
+    """
+    def __init__(self, nAvg, nLat, **kwargs):
+        self.nAvg = nAvg
+        self.nLat = nLat
+        super(Rec, self).__init__(**kwargs)
+
+    @property
+    def _format(self):
+        return "%df%di" % (self.nAvg, self.nLat)
+
+    def initEmpty(self):
+        self.avg = [0] * max(self.nAvg, monm0)
+        self.lat = [0] * max(self.nLat, 7)
+
+    def getElements(self):
+        return self.avg[:self.nAvg] + self.lat[:self.nLat]
+
+    def setFromElements(self, elements):
+        self.initEmpty()
+        self.avg[:self.nAvg] = list(elements[:self.nAvg])
+        self.lat[:self.nLat] = list(elements[self.nAvg:self.nAvg + self.nLat])
+
+
+class TrimmedRec(Rec):
+    def __init__(self, nAvg, nLat, **kwargs):
+        super(TrimmedRec, self).__init__(nAvg, nLat, **kwargs)
+        self.mln = 0
+
+    @property
+    def _format(self):
+        return "i7i%df" % (self.nAvg, )
+
+    def getElements(self):
+        return [self.mln] + self.lat[:7] + self.avg[:self.nAvg]
+
+    def set_from_rec(self, rec):
+        self.nAvg = rec.nAvg
+        self.nLat = rec.nLat
+        self.avg = list(rec.avg)
+        self.lat = list(rec.lat)
+
+
+def findl6(avg, ml, xbad):
+    """Find the value for element 6 of the LAT array.
+
+    This counts the number of values in ``avg[:ml]`` that are good (i.e.
+    not equal to ``xbad``). This is the return value and is the value to
+    be used for the 6th element in the ``LAT`` array in an output record.
+
+    :Return:
+        The number of good average values.
+
+    """
+    return len([el for el in avg[:ml] if el != xbad])
+
+
+def main(args):
+    """The main for this module.
+
+    """
+    if len(args) < 1:
+        f10 = fort.open("work/fort.10")
+    else:
+        f10 = fort.open(args[0])
+    if len(args) < 2:
+        f11 = fort.open("work/fort.11", "wb")
+    else:
+        f11 = fort.open(args[1], "wb")
+    h = ccc_binary.CCHeader2(f10.readline())
+
+    km = 1
+    if h.mavg == 6:
+        km = 12
+    if h.mavg == 7:
+        km = 4
+    # Here Fortran code verifies that the input file gives km=12, but that will
+    # always be true for the CCC code.
+    if km != km0:
+        sys.exit("ERROR: CHANGE KM0")
+
+    ml = h.nm
+    if monm0 < ml:
+        sys.exif("Set monm0 at least to %s" % ml)
+
+    mbad = last = h.bad
+    xbad = float(mbad)
+    newHdr = h.copy()
+    newHdr.recsize = h.nm + 8
+    nLat = h.recsize - h.nm
+
+    rec = Rec(ml, nLat)
+    reco = TrimmedRec(ml, nLat)
+    rec.lat[4:6] = [0, 1, 0]
+    reco.lat[4:6] = [0, 1, 0]
+    temp_rec = Rec(ml, nLat)
+    temp_rec.setFromBinary(data=f10.readline())
+    reco.set_from_rec(temp_rec)
+    newHdr.ml = 1
+
+    if nLat == 4:
+        reco.lat[5] = findl6(reco.avg, ml, xbad)
+    # If first SBBX has data then set ``newHdr.ml`` to ``ml``.
+    if reco.lat[5] > 0:
+        newHdr.ml = ml
+
+    f11.writeline(newHdr.binary)
+
+    for n in range(2, nsbbx + 1):
+        rec = Rec(ml, nLat, data=f10.readline())
+        reco.mln = 1
+        reco.nAvg = ml
+        if nLat == 4:
+            rec.lat[5] = findl6(rec.avg, ml, xbad)
+        if rec.lat[5] > 0:
+            reco.mln = ml
+        if reco.lat[5] == 0:
+            data = tuple([reco.mln] + reco.lat + [xbad])
+            bin = struct.pack("i7if", *data)
+            f11.writeline(bin)
+        else:
+            f11.writeline(reco.binary)
+        reco.set_from_rec(rec)
+        reco.nAvg = ml
+
+    reco.mln = 0
+    if reco.lat[5] == 0:
+        data = tuple([reco.mln] + reco.lat + [xbad])
+        bin = struct.pack("i7if", *data)
+        f11.writeline(bin)
+    else:
+        f11.writeline(reco.binary)
+
+
+if __name__ == "__main__":
+    import optparse
+    usage = "usage: %prog [options] [infile] [outfile]"
+    usage += "\n\nThe infile, outfile default to work/fort.10, work/fort.11"
+    parser = script_support.makeParser(usage)
+    options, args = script_support.parseArgs(parser, __doc__, (0, 2))
+    main(args)

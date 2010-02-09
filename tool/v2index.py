69a1745a20f6295db2be3aed487e9603f1ef9ed7
drj@pobox.com <drj@pobox.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2010-02-09 14:46:36 +0000

    v2index: File class: implements random access to v2.mean records by 11-digit or 12-digit identifier.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@255 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- tool/v2index.py
+++ tool/v2index.py
@@ -12,6 +12,7 @@ to have faster random access.
 """
 
 import itertools
+import sys
 
 def build(inp, out):
     """Build an index of the v2.mean file `inp` and write it to the file
@@ -45,8 +46,123 @@ def tell_stream(f):
             break
         yield whence, line
 
+class File:
+    """A v2.mean file accessible randomly using an index."""
+
+    def __init__(self, file):
+        """`file` can be an open file object, as long as it has the
+        .name attribute, or a filename."""
+
+        try:
+            self.name = file.name
+        except:
+            self.name = file
+        self.indexname = self.name + '.index'
+        try:
+            self.index = index(open(self.indexname))
+        except IOError:
+            self.build()
+        try:
+            file.name
+            self.file = file
+        except:
+            self.file = open(self.name)
+
+    def build(self):
+        """(Re-) build the index file.  Updates self.index."""
+
+        sys.stderr.write("Building index...\n")
+        inp = open(self.name)
+        out = open(self.indexname, 'w')
+        build(inp, out)
+        out.close()
+        inp.close()
+        sys.stderr.write("Done building index...\n")
+        self.index = index(open(self.indexname))
+
+    def get_id12(self, id12):
+        """For a given 12-digit record identifier, return an iterator
+        that yields each datum.
+        """
+
+        assert 12 == len(id12)
+
+        # Get the first line according to the index, and check it
+        rebuiltindex = False
+        while True:
+            i = self.index[id12]
+            self.file.seek(i.whence)
+            first = self.file.readline()
+            if first[:16] != i.match:
+                if rebuiltindex:
+                    raise Error(
+                      "Index still wrong after rebuilding.  id12=%s" % id12)
+                sys.stderr.write("Index is wrong, rebuilding it...\n")
+                rebuiltindex = True
+                self.build()
+                continue # goto, really
+            break
+
+        def iter():
+            """Iterator for the lines."""
+
+            line = first
+            while line[:12] == id12:
+                yield line
+                line = self.file.readline()
+        return iter()
+
+    def get_id11(self, id11):
+        """For a given 11-digit station identifier, return a sequence of
+        (id12, series) pairs where each corresponds to a 12-digit record
+        identifier, and each series yields the data for that record.
+        """
+
+        assert 11 == len(id11)
+
+        for id12 in self.index[id11]:
+            yield (id12, self.get_id12(id12))
+
+    def get(self, id):
+        """As get_id11, but either 11-digit or 12-digit identifier is
+        allowed.  If a 12-digit identifier is used then the sequence
+        returned has only one pair.
+        """
+
+        if 11 == len(id):
+            for pair in self.get_id11(id):
+                yield pair
+        else:
+            assert 12 == len(id)
+            yield (id, self.get_id12(id))
+
+def index(f):
+    """Read the index file `f` and return a dictionary that maps from
+    id12 to an Index object, and also maps id11 to a list of id12s.
+    """
+
+    d = {}
+    for line in f:
+        i = Index(line)
+        d[i.id12] = i
+        # Append the id12 to a list associated with each id11, creating
+        # a list if necessary.
+        id11 = i.id12[:11]
+        v = d.setdefault(id11, [])
+        v.append(i.id12)
+    return d
+
+class Index:
+    """A single entry from the index file."""
+
+    def __init__(self, line):
+        self.id12, self.year, self.whence = line.split()
+        # Concatentate these two to form the 16 character string that
+        # must match the v2.mean file at the specified place.
+        self.match = self.id12 + self.year
+        self.whence = int(self.whence)
+
 def main(argv=None):
-    import sys
     if argv is None:
         argv = sys.argv
     return build(open('input/v2.mean'), open('work/v2.mean.index', 'w'))

c5118f52117c70db8a5d6be3347a4cffbc2e5f41
drj@pobox.com <drj@pobox.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2010-03-20 15:43:43 +0000

    step3 output to SVG grid display tool.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@419 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

---
+++ tool/grid.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# $URL$
+# $Rev$
+#
+# grid.py
+
+"""
+grid YYYY-MM [v2-file]
+
+Display gridded anomalies as SVG file.
+"""
+
+def map(when, inp, out):
+    """Take a ccc-gistemp subbox file in V2 mean format as *inp* and
+    produce an SVG file on *out*."""
+
+    import math
+    import re
+
+    out.write("""<svg 
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      version="1.1">\n""")
+
+    m = re.match(r'(\d{4})-(\d{2})', when)
+    year, month = m.groups()
+    # Convert to 0-based month.
+    month = int(month) - 1
+    assert 0 <= month < 12
+
+    for (lat,lon),v in filter_month(inp, year, month):
+        x,y = topixel(lat, lon)
+        if v > 0:
+            fill = 'red'
+        else:
+            fill = 'blue'
+        r = math.sqrt(abs(v))
+        out.write("""<circle cx='%.1f' cy='%.1f' r='%.1f' fill='%s' />\n""" %
+          (x, y, r, fill))
+    out.write("""</svg>\n""")
+
+def topixel(lat, lon):
+    u"""Return x,y coordinate.  Plate Carr\xe9e projection."""
+
+    return lon,lat
+
+
+def filter_month(inp, year, month):
+    """Yield each value from the v2.mean file for the year and month in
+    question.  Invalid values (marked with -9999 in the file) are
+    ignored.  Each value is yielded as ((lat,lon),v) where lat,lon are
+    the latitude and longitude in degrees (positive is North and East).
+    """
+
+    for line in inp:
+        if line[12:16] == year:
+            v = int(line[16+5*month:21+5*month])
+            if v != -9999:
+                lat = float(line[:5])
+                lon = float(line[5:11])
+                yield ((lat,lon), v)
+
+def main(argv=None):
+    import sys
+    if argv is None:
+        argv = sys.argv
+
+    when = argv[1]
+    if len(argv) > 2:
+        f = open(argv[2])
+    else:
+        f = sys.stdin
+    map(when, f, sys.stdout)
+
+if __name__ == '__main__':
+    main()

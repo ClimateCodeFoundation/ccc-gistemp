b9118516fcc32389d9ec76d4b8785ba02da0920d
drj@pobox.com <drj@pobox.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2010-08-24 18:20:37 +0000

    ukmetv2: Scrape Met Office website to get temperature data.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@515 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- tool/ukmetv2.py
+++ tool/ukmetv2.py
@@ -10,6 +10,7 @@
 import itertools
 import re
 import sys
+import urllib
 
 # Requires Python 2.5.
 assert sys.version_info[:2] >= (2,5)
@@ -22,6 +23,8 @@ import giss_io
 urls="""
 http://www.metoffice.gov.uk/climate/uk/stationdata/oxforddata.txt
 """.split()
+
+PAGE = """http://www.metoffice.gov.uk/climate/uk/stationdata/"""
 
 def scrapeit(prefix):
     """Creates files of the form:
@@ -40,8 +43,28 @@ def scrapeit(prefix):
     out.max = giss_io.V2MeanWriter(path=prefix + '.v2.max')
     out.inv = open(prefix + '.v2.inv', 'w')
 
-    for url in urls:
+
+    for url in getlocations():
+        print "Data from", url, "..."
         scrape1(url, out)
+
+def getlocations():
+    """Return a sequence of URLs, each one being a text file containing
+    temperature data.  Scraped from "Historic station data" of the UK
+    Met Office."""
+
+    # http://docs.python.org/release/2.4.4/lib/module-urlparse.html
+    import urlparse
+
+    from BeautifulSoup import BeautifulSoup
+
+    text = urllib.urlopen(PAGE).read()
+    souped = BeautifulSoup(text)
+
+    for element in souped.findAll('option', value=re.compile(r'\.txt$')):
+        u = element['value']
+        url = urlparse.urljoin(PAGE, u)
+        yield url
 
 LARGE = 8888
 
@@ -51,27 +74,34 @@ def scrape1(url, out):
     http://www.metoffice.gov.uk/climate/uk/stationdata/yeoviltondata.txt
     """
 
-    import urllib
+    meta = False
     f = urllib.urlopen(url)
     name = f.readline()
-    id11 = "UKMET" + name[:6].upper()
-    id12 = id11 + '0'
-    location = f.readline()
-    assert location.startswith('Location:')
-    loc = location.split()[1:3]
-    loc[1] = loc[1].strip(',')
-    # Sort by last char, so that easting comes first.
-    loc.sort(key=lambda x:x[-1])
-    assert loc[0].endswith('E')
-    assert loc[1].endswith('N')
-    loc = [x[:-1] for x in loc]
-    # Extend to 6 figures (metres)
-    loc = [x + '0'*(6-len(x)) for x in loc]
-    # Convert to number, metres.
-    easting, northing = map(int, loc)
-    # Don't forget to use http://gridconvert.appspot.com/ to convert
-    # from OSGB to GRS80.
-    # :todo: save metadata instead of ignoring it.
+    try:
+        shortname = name.replace(' ', '')
+        shortname = (shortname + '_'*6)[:7]
+        assert 7 == len(shortname)
+        id11 = "UKMO" + shortname.upper()
+        id12 = id11 + '0'
+        location = f.readline()
+        assert location.lower().startswith('location')
+        loc = location.split()[1:3]
+        loc[1] = loc[1].strip(',')
+        # Sort by last char, so that easting comes first.
+        loc.sort(key=lambda x:x[-1])
+        assert loc[0].endswith('E')
+        assert loc[1].endswith('N')
+        loc = [x[:-1] for x in loc]
+        # Extend to 6 figures (metres)
+        loc = [x + '0'*(6-len(x)) for x in loc]
+        # Convert to number, metres.
+        easting, northing = map(int, loc)
+        # Don't forget to use http://gridconvert.appspot.com/ to convert
+        # from OSGB to GRS80.
+        # :todo: save metadata instead of ignoring it.
+    except AssertionError, err:
+        print "Skipping metadata"
+        print err
 
     def year(row):
         """Extract the year from a row."""

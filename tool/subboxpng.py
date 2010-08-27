f6fe06d2d49adef6ae5cf914d7b148e2b9ce7891
drj@pobox.com <drj@pobox.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2010-08-27 22:31:37 +0000

    subboxpng: more advanced modularity.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@537 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- tool/subboxpng.py
+++ tool/subboxpng.py
@@ -30,8 +30,10 @@ import png
 import extend_path
 from code import eqarea
 
-def topng(inp):
-    """Convert a step5mask into a PNG file."""
+def topng(inp, date=None):
+    """Convert *inp* into a PNG file.  Input file can be a step5mask
+    file (produces greyscale PNG), or if *date* is supplied it can be a
+    subbox file."""
 
     # :todo: move into proper module.
     from landmask import centrein
@@ -46,14 +48,13 @@ def topng(inp):
     # an array of rows:
     a = [[0]*width for _ in range(height)]
 
-    for row,box in itertools.izip(inp, eqarea.grid8k()):
-        lat = float(row[:5])
-        lon = float(row[5:11])
-        s,n,w,e = box
-        # If either of these fail, the input mask is in wrong sequence.
-        assert s < lat < n
-        assert w < lon < e
-        v = float(row[16:21])
+    cells = eqarea.grid8k()
+    if not date:
+        values = maskbybox(inp, cells)
+    else:
+        values = extractdate(inp, cells, date)
+
+    for v,box in values:
         v = 255-int(round(v*255))
         for x,y in centrein(box, resolution):
             a[y][x] = v
@@ -67,18 +68,34 @@ def topng(inp):
       bitdepth=8)
     w.write(open(outpath, 'wb'), a)
 
+def maskbybox(inp, grid):
+    """Read a step5mask file box by box.  Yield (value, box) pair.
+    """
+    for row,box in itertools.izip(inp, grid):
+        lat = float(row[:5])
+        lon = float(row[5:11])
+        s,n,w,e = box
+        # If either of these fail, the input mask is in wrong sequence.
+        assert s < lat < n
+        assert w < lon < e
+        v = float(row[16:21])
+        yield v, box
+
 def main(argv=None):
     import sys
-    # http://docs.python.org/release/2.4.4/lib/module-fileinput.html
-    import fileinput
+    import getopt
 
     if argv is None:
         argv = sys.argv
 
-    arg = argv[1:]
+    k = {}
+    opt, arg = getopt.getopt(argv[1:], '', ['date'])
+    for o,v in opt:
+        if o == '--date':
+            k['date'] = v
     if arg:
         for p in arg:
-            topng(open(p))
+            topng(open(p), **k)
     else:
         topng(sys.stdin)
 

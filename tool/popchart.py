938171407f2da5c3a4be8dc0d1ccfa960f488391
drj@pobox.com <drj@pobox.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2010-10-28 19:05:56 +0000

    popchart.py: minor doc improvement.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@591 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- tool/popchart.py
+++ tool/popchart.py
@@ -5,9 +5,15 @@
 # popchart.py
 #
 # David Jones, Climate Code Foundation, 2010-10-28
+#
+# REFERENCES
+#
+# P. D. Jones and A. Moberg;
+# "Hemispheric and Large-Scale Surface Air Temperature Variations: An
+# Extensive Revision and an Update to 2001"; Journal of Climate; 2003.
 
 """Tool to draw a googlechart of year versus number of data in that
-year."""
+year.  See Jones and Moberg 2003 Figure 1 for an example."""
 
 import itertools
 import urllib
@@ -41,8 +47,9 @@ def popchart(inp, out):
     out.write(url+'\n')
 
 def limit(x):
-    """Calculate a limit.  The result is either 1, 2, or 5 times some
-    power of ten.  And is the smallest such number that is >= x.
+    """Calculate a limit (for the scale of the y-axis).  The result
+    is either 1, 2, or 5 times some power of ten.  And is the smallest
+    such number that is >= x.
     """
 
     if x < 2:

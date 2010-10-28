83186172a4bb0730a024bde4ca98fecb6db2f456
drj@pobox.com <drj@pobox.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2010-10-28 10:32:40 +0000

    cascrape.py: Write output into input/ca.v2.mean ca.json ca.v2.inv
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@586 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- tool/cascrape.py
+++ tool/cascrape.py
@@ -13,7 +13,7 @@ Tool to collect temperature data for Candada scraped by ScraperWiki.
 import json
 import urllib
 
-def gather(scraper, v2out, v2inv, log):
+def gather(scraper, v2out, v2inv, jsoninv, log):
     """Takes the name of a scraper hosted at ScraperWiki and gathers all
     its data."""
 
@@ -25,28 +25,40 @@ def gather(scraper, v2out, v2inv, log):
     # we have got them all.
     limit = 500
     offset = 0
+    meta = []
+    rows = []
+    count = 0
     while 1:
         url = api + '?' + urllib.urlencode(dict(name=name,
             format='json', limit=limit, offset=offset))
+        # We terminate when the JSON request returns no results, which
+        # we detect using this flag.
         gotnone = True
         result = json.load(urllib.urlopen(url))
         for item in result:
+            gotnone = False
+            count += 1
+            log.write('\r%d records' % count)
+            log.flush()
             if 'meta' in item:
-                log.write(item['meta'])
-                log.write('\n')
-                meta = json.loads(item['meta'])
-                v2inv.write(formatmetav2(meta))
+                meta.append(json.loads(item['meta']))
                 continue
             # Make a row in GHCN v2 format.  Add a '0' to make a
             # 12-character record identifier.
-            row = "%s0%s%s\n" % (item['id'], item['year'], item['tmean'])
-            v2out.write(row)
-            gotnone = False
+            id11 = item['id']
+            assert 11 == len(id11)
+            row = "%s%s%s\n" % (id11+'0', item['year'], item['tmean'])
+            rows.append(row)
         if gotnone:
             break
         offset += 500
         # Just a sanity check
         assert offset < 1e5
+    log.write('\n')
+    v2out.writelines(sorted(rows))
+    for m in sorted(meta, key=lambda x: x['id11']):
+        jsoninv.write(json.dumps(m) + '\n')
+        v2inv.write(formatmetav2(m))
 
 def formatmetav2(meta):
     """Format a metadata dict as a row in the same format as the GHCN
@@ -72,8 +84,9 @@ def main(argv=None):
         argv = sys.argv
 
     gather('canada-climate-data',
-      v2out=open('ca.v2', 'w'),
-      v2inv=open('ca.v2.inv', 'w'),
+      v2out=open('input/ca.v2.mean', 'w'),
+      v2inv=open('input/ca.v2.inv', 'w'),
+      jsoninv=open('input/ca.json', 'w'),
       log=sys.stdout)
 
 if __name__ == '__main__':

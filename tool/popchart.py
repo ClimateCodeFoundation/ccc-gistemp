0c45d09874a387bf91994ae02138b5e40c0ab3b8
drj@pobox.com <drj@pobox.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2010-11-02 10:36:24 +0000

    popchart.py: can now take multiple inputs.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@607 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- tool/popchart.py
+++ tool/popchart.py
@@ -20,33 +20,51 @@ import urllib
 
 prefix = 'http://chart.apis.google.com/chart'
 
-def popchart(inp, out):
-    """Output googlechart URL on *out*."""
+def popchart(inps, out):
+    """Output googlechart URL on *out*. *inps* is a list of files in
+    v2.mean format."""
 
-    counts = count(inp)
-    miny = min(counts)
-    maxy = max(counts)
-    most = max(counts.values())
-    l = [counts.get(y, 0) for y in range(miny,maxy+1)]
-    data = ','.join(map(str, l))
-    xlimit = limit(most)
+    # There is one "count" dict for each input, the dict maps from year
+    # to count of rows for that year.
+    counts = [count(inp) for inp in inps]
+    minyear = min(min(c) for c in counts)
+    maxyear = max(max(c) for c in counts)
+    most = max(max(c.values()) for c in counts)
+    yscale = reasonable_scale(most)
+
+    # Convert the counts to simple sequences (each sequence starting with
+    # *minyear*.
+    seqs = [[c.get(y, 0) for y in range(minyear,maxyear+1)]
+      for c in counts]
+
+    data = '|'.join(','.join(map(str, l)) for l in seqs)
+
+    chdl=None
+    try:
+        chdl = '|'.join(inp.name for inp in inps)
+    except:
+        pass
 
     d = dict(cht='bvg',
       chd='t:'+data,
+      chdl=chdl,
       chxt='x,y,x',
       chxl='2:|year+(CE)',
       chxp='2,50',
       chs='440x330',
-      chds="1,%d" % xlimit,
+      chds="1,%d" % yscale,
       chbh='a,0,2',
       chco='6611cc',
-      chxr='0,%d,%d,10|1,0,%d' % (miny,maxy,xlimit),
+      chxr='0,%d,%d,10|1,0,%d' % (minyear,maxyear,yscale),
     )
+
+    if not d['chdl']:
+        del d['chdl']
 
     url = prefix + '?' + '&'.join(map('='.join, d.items()))
     out.write(url+'\n')
 
-def limit(x):
+def reasonable_scale(x):
     """Calculate a limit (for the scale of the y-axis).  The result
     is either 1, 2, or 5 times some power of ten.  And is the smallest
     such number that is >= x.
@@ -64,7 +82,8 @@ def limit(x):
 
 def count(inp):
     """*inp* is a file in v2.mean format.  Counts the number of rows in
-    each year."""
+    each year.  The result is a dict that maps from year (a number) to
+    count (also a number)."""
 
     def getyear(row):
         return row[12:16]
@@ -79,10 +98,10 @@ def main(argv=None):
 
     arg = argv[1:]
     if arg:
-        f = open(arg[0])
+        fs = map(open, arg)
     else:
-        f = sys.stdin
-    popchart(f, sys.stdout)
+        fs = [sys.stdin]
+    popchart(fs, sys.stdout)
 
 if __name__ == '__main__':
     main()

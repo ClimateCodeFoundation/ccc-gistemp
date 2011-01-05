0a39a4c304ba14df396edeecb694ab9a91c23ef7
drj@ravenbrook.com <drj@ravenbrook.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2011-01-05 17:24:21 +0000

    multi.py cellstations: now considers weights and ordering of stations.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@665 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- tool/multi.py
+++ tool/multi.py
@@ -21,7 +21,8 @@ class Fatal(Exception):
 
 def cellstations(arg):
     """[command] Compares the lists of stations for each cell.  arg[1]
-    and arg[2] should be the names of two different log/step3.log files."""
+    and arg[2] should be the names of two different log/step3.log files.
+    """
 
     fs = map(open, arg[1:3])
     stations = map(celldict, fs)
@@ -30,26 +31,59 @@ def cellstations(arg):
     common = set(stations[0]) & set(stations[1])
     print "%d common cells" % len(common)
     for cell in common:
-        if stations[0][cell] != stations[1][cell]:
-            ina = stations[0] - stations[1]
-            inb = stations[1] - stations[0]
+        # Get the two collections of stations...
+        # as lists...
+        stationsa = stations[0][cell]
+        stationsb = stations[1][cell]
+        # as dicts...
+        dicta = dict(stationsa)
+        dictb = dict(stationsb)
+        # and sets.
+        seta = set(dicta)
+        setb = set(dictb)
+        if stationsa != stationsb:
+            ina = seta - setb
+            inb = setb - seta
             if ina:
                 print "Cell %s %s only " % (cell, fs[0].name,
                   ' '.join(sorted(ina)))
             if inb:
                 print "Cell %s %s only " % (cell, fs[1].name,
                   ' '.join(sorted(inb)))
+            commonstations = seta & setb
+            for station in commonstations:
+                if dicta[station] != dictb[station]:
+                    print "Cell %s station %s has different weights (%f and %f)." %(
+                      cell, station, dicta[station], dictb[station])
+            # Split into stations with 0 weight and those with non-zero
+            # weight.
+            zeroa = [s for s,w in stationsa if not w and s in commonstations]
+            zerob = [s for s,w in stationsb if not w and s in commonstations]
+            posa = [s for s,w in stationsa if w and s in commonstations]
+            posb = [s for s,w in stationsb if w and s in commonstations]
+            for i,(a,b) in enumerate(zip(posa, posb)):
+                if a != b:
+                    print "Cell %s order differs: %s and %s at index %d" % (
+                       cell, a, b, i)
+                break
+            # Not normally reported.
+            if False and zeroa != zerob:
+                print "Cell %s (the unused stations are in different order; result not affected)" % (
+                  cell)
+
 
 def celldict(f):
     """From the (open) step3.log file *f* return a dict that maps
-    from cell identifier (12 characters) to a set of stations."""
+    from cell identifier (12 characters) to a list of (station,weight)
+    pairs.
+    """
 
     result = {}
     for row in f:
         row = row.split(' ', 2)
         if row[1] != 'stations':
             continue
-        stations = set(station for station,weight_ in json.loads(row[2]))
+        stations = json.loads(row[2])
         result[row[0]] = stations
     return result
 

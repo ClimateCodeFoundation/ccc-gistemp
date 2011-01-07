8f81fab59431304dac8cb676cc036fcc9e215224
drj@ravenbrook.com <drj@ravenbrook.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2011-01-07 11:12:07 +0000

    multi.py: blog20110106 helps draw a specific KML file.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@666 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- tool/multi.py
+++ tool/multi.py
@@ -12,6 +12,8 @@ scripts.
 Run "python tool/multi.py commands" to see command list.
 """
 
+import extend_path
+
 # http://docs.python.org/release/2.6.6/library/json.html
 import json
 import re
@@ -86,6 +88,141 @@ def celldict(f):
         stations = json.loads(row[2])
         result[row[0]] = stations
     return result
+
+
+def blog20110106(arg):
+    """Program for drawing circle for blog post.  The circle required is
+    all points with 1200 km of a particular point on the Earth's
+    surface."""
+
+    from code import earth
+
+    p = (64.4,-90)
+    d = 1200.0/earth.radius
+
+    l = surfcircle(p, d)
+    l.append(l[0])
+    frag = kmlcoord(l)
+    print frag
+
+def kmlcoord(l):
+    """Convert list of (lat,lon) pairs to a list suitable for the
+    contents of a KML <coordinates> element.
+    """
+
+    return ' '.join(','.join(map(str, [p[1],p[0],0])) for p in l)
+
+def surfcircle(point, arc):
+    """Compute the circle that is *arc* radians from *point* (which is a
+    (lat,lon) pair in degrees).  A list is returned which is a polygon
+    that approximates the circle.  The returned list is a list of
+    (lat,lon) pairs (in degrees); the elements of the list are distinct
+    (in particular, the last element is not equal to the first).
+    """
+
+    import math
+
+    # Proceed by:
+    # - Choose some point *q*, where the angle between *point* and
+    #     *q* is *arc* radians.  Convert *q* to 3D cartesian coords.
+    # - Construct the 3x3 matrix that is a rotation about the vector
+    #     through *point*, rotating through an angle of *s* degrees (s = 10,
+    #     say).
+    # - Repeatedly apply the matrix to *q* to get the orbit of *q* under
+    #     the rotation.
+    # - The list of points in the orbit is the required polygon; convert
+    #   to (lat,lon) representation.
+
+    s = 10
+
+    p = [math.radians(x) for x in point]
+    # If p is in Northern Hemisphere, pick q to be *arc* radians South;
+    # and pick q to be *arc* radians North when p is in Southern
+    # Hemisphere.
+    if p[0] >= 0:
+        q = (p[0]-arc, p[1])
+    else:
+        q = (p[0]+arc, p[1])
+
+    pcart = cartesian3D(p)
+    qcart = cartesian3D(q)
+
+    s = 10
+    th = math.radians(s)
+    rot = m3x3rotVth(pcart, th)
+
+    result = []
+    for _ in range(0,360,s):
+        result.append(qcart)
+        qcart = m3x3multV(rot, qcart)
+
+    return converttolatlon(result)
+
+def converttolatlon(l):
+    """Convert a list of (x,y,z) cartesian triples to list of (lat,lon)
+    pairs (in degrees)."""
+
+    return [convert1latlon(p) for p in l]
+
+def convert1latlon(p):
+    """Convert *p* from a cartesian triple (x,y,z) to (lat,lon) pair in
+    degrees."""
+
+    assert 0.99 < sum(v*v for v in p)**0.5 < 1.01
+
+    import math
+
+    x,y,z = p
+
+    lon = math.atan2(y,x)
+    lat = math.asin(z)
+
+    return [math.degrees(v) for v in (lat,lon)]
+
+def cartesian3D(p):
+    """Convert *p*, which is a (lat,lon) pair in radians, to 3D
+    cartesian coordinates."""
+
+    import math
+
+    lat,lon = p
+    x = math.cos(lon) * math.cos(lat)
+    y = math.sin(lon) * math.cos(lat)
+    z = math.sin(lat)
+    return [x,y,z]
+
+def m3x3rotVth(v, theta):
+    """Return a 3x3 rotation matrix for a rotation of *theta* radians
+    around vector *v*.  In a right-handed systen, positive *theta* gives
+    a counter-clockwise rotation when viewed along the line from *v* to
+    the origin.
+    """
+
+    import math
+
+    # From
+    # http://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
+
+    c = math.cos(theta)
+    s = math.sin(theta)
+    d = 1-c
+    x,y,z = v
+
+    R = [[c+x**2*d,  x*y*d-z*s, x*z*d+y*s],
+         [y*x*d+z*s, c+y**2*d,  y*z*d-x*s],
+         [z*x*d-y*s, z*y*d+x*s, c+z**2*d]]
+    return R
+
+def m3x3multV(M, v):
+    """Multiply the vector *v* by the 3x3 matrix *M*: result is Mv."""
+
+    return [VdotV(row,v) for row in M]
+
+def VdotV(u, v):
+    """Dot product of *u* and *v*."""
+
+    return sum(x*y for x,y in zip(u,v))
+
 
 def commands(arg):
     """[command] Show command list."""

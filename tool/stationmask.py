8c8c191df914779f4a1e5a5c11ae8559b24f7da0
drj11x@googlemail.com <drj11x@googlemail.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2011-08-15 13:06:10 +0000

    Can select stations from another file.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/trunk@925 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- tool/stationmask.py
+++ tool/stationmask.py
@@ -7,21 +7,28 @@
 # David Jones, Clear Climate Code, 2010-08-27
 
 """
-stationmask.py cells
+stationmask.py cells [stations]
 
-From the input/v2.inv file extract a list of stations that are in a
-selected set of cells.  The subbox cells are taken from the cells file
-which should be in the same format as step5mask (value 0.000 excludes
-that cell, any other value includes it).
+Extract a list of stations that are in a selected set of cells.  The subbox
+cells are taken from the *cells* file which should be in the same
+format as step5mask (value 0.000 excludes that cell, any other value
+includes it).  For rectangular regions, you can produce a suitable mask
+file with tool/rectmask.py.
+
+The locations of the stations are taken from the input/v2.inv file.  The
+list of stations is taken from the *stations* file, using input/v2.inv
+when this is not specified.
 """
 
 # Clear Climate Code
 import extend_path
 from code import eqarea
 
-def stationmask(inp, out, inv, dribble):
-    """Take list of cells from *inp*, and output on *out* a list of
-    station identifiers (11-digit), taken from the v2.inv file *inv*.
+def stationmask(inp, stations, out, inv, dribble):
+    """Take list of cells from *inp*, list of stations from *stations*,
+    and output on *out* a list of station identifiers (11-digit) (that
+    are within the cells specified in *inp); locations are taken from
+    the v2.inv file *inv*.
     """
 
     centres = [row[:11] for row in inp if float(row[16:21])]
@@ -31,14 +38,21 @@ def stationmask(inp, out, inv, dribble):
     selectboxes = [box for box in boxes if
       [centre for centre in centres if boxcontains(box, centre)]]
     print >> dribble, len(selectboxes), "boxes"
-    stations = list(inv)
+    # Set of stations.
+    stations = set(l[:11] for l in stations)
+    meta = list(inv)
     selected = []
-    for line in stations:
+    # Iterate over all entries in v2.inv, discarding those not in
+    # *stations*.
+    for line in meta:
+        uid = line[:11]
+        if uid not in stations:
+            continue
         lat = float(line[43:49])
         lon = float(line[50:57])
         for box in selectboxes:
             if boxcontains(box, (lat,lon)):
-                selected.append(line[:11])
+                selected.append(uid)
                 dribble.write('\r%d stations' % len(selected))
                 break
     dribble.write('\n')
@@ -58,9 +72,14 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv
     arg = argv[1:]
+    if len(arg) > 1:
+        stations = arg[1]
+    else:
+        stations = 'input/v2.inv'
 
-    stationmask(open(arg[0]), sys.stdout, open('input/v2.inv'),
-      sys.stderr)
+    stationmask(open(arg[0]), stations=open(stations),
+      out=sys.stdout, inv=open('input/v2.inv'),
+      dribble=sys.stderr)
 
 if __name__ == '__main__':
     main()

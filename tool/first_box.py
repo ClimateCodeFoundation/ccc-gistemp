43f49f3ef063f0f1df11e2901bd15d10b1f68451
David Jones <drj@pobox.com>
2014-09-02 16:41:37 +0100

    add 1 to box numbers and put in separate function

--- tool/first_box.py
+++ tool/first_box.py
@@ -4,6 +4,15 @@
 Show the first year that each box has data.
 """
 
+# REFERENCES
+
+# [HANSENLEBEDEFF1987]
+#   "Global Trends of Measured Surface Air Temperature"
+#   Journal of Geophysical Research, Vol. 92, No. D11,
+#   Pages 13,345-13,372;
+#   1987-11-20
+
+
 import glob
 import itertools
 import os
@@ -17,7 +26,13 @@ def box_starts(inp):
     def get_box(p):
         return p[0][0]
     for box, data in itertools.groupby(box_series(inp), get_box):
-        print box, list(data)[0][0][1]
+        yield box, list(data)[0][0][1]
+
+def as_text(data):
+    for box, year in data:
+        # To be commensurate with [HANSENLEBEDEFF1987] figure 2,
+        # we start numbering boxes from 1.
+        print box+1, year
 
 def main(argv=None):
     if argv is None:
@@ -26,7 +41,7 @@ def main(argv=None):
     box_file = glob.glob(os.path.join('result', 'landBX*'))[0]
     sys.stderr.write("Using %s\n" % box_file)
     with open(box_file, 'rb') as inp:
-        box_starts(inp)
+        as_text(box_starts(inp))
 
 
 if __name__ == '__main__':
